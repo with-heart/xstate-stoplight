8e4720073d729db68572e2769a3ea007346af8c9
with-heart <with.heart+git@pm.me>
2022-11-27 22:43:46 -0500

    add colors to lights

--- src/stoplight.tsx
+++ src/stoplight.tsx
@@ -1,9 +1,9 @@
 export const Stoplight = () => {
   return (
     <div>
-      <div className="light" />
-      <div className="light" />
-      <div className="light" />
+      <div className="light" data-color="red" />
+      <div className="light" data-color="yellow" />
+      <div className="light" data-color="green" />
     </div>
   )
 }
