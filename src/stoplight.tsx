a8478093ef779d204e4f5ceed7a6f435cb958757
with-heart <with.heart+git@pm.me>
2022-11-28 23:51:10 -0500

    add stoplight frame

--- src/stoplight.tsx
+++ src/stoplight.tsx
@@ -1,6 +1,6 @@
 export const Stoplight = () => {
   return (
-    <div>
+    <div className="stoplight">
       <div className="light" data-color="red" data-active={false} />
       <div className="light" data-color="yellow" data-active={false} />
       <div className="light" data-color="green" data-active={false} />
