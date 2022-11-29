c8070643e8f164027f9b77c318c88ddd6b0ddf34
with-heart <with.heart+git@pm.me>
2022-11-28 22:39:16 -0500

    add bright light styles

--- src/stoplight.tsx
+++ src/stoplight.tsx
@@ -1,9 +1,9 @@
 export const Stoplight = () => {
   return (
     <div>
-      <div className="light" />
-      <div className="light" />
-      <div className="light" />
+      <div className="light" data-active={false} />
+      <div className="light" data-active={false} />
+      <div className="light" data-active={false} />
     </div>
   )
 }
