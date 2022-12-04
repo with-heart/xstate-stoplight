09f7861bdac71e80254c60b5f00ef057a532ed8d
with-heart <with.heart+git@pm.me>
2022-12-04 15:59:36 -0500

    wrap aspects in stoplight

--- src/stoplight.tsx
+++ src/stoplight.tsx
@@ -2,7 +2,7 @@ import styles from './stoplight.module.scss'
 
 export const Stoplight = () => {
   return (
-    <div>
+    <div className={styles.stoplight}>
       <div className={styles.aspect} data-aspect="red" />
       <div className={styles.aspect} data-aspect="amber" />
       <div className={styles.aspect} data-aspect="green" />
