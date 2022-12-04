c0fdef9579eea23e86c110f3f20b22a88465582b
with-heart <with.heart+git@pm.me>
2022-12-04 15:43:51 -0500

    assign color to each aspect

--- src/stoplight.tsx
+++ src/stoplight.tsx
@@ -3,9 +3,9 @@ import styles from './stoplight.module.scss'
 export const Stoplight = () => {
   return (
     <div>
-      <div className={styles.aspect} />
-      <div className={styles.aspect} />
-      <div className={styles.aspect} />
+      <div className={styles.aspect} data-aspect="red" />
+      <div className={styles.aspect} data-aspect="amber" />
+      <div className={styles.aspect} data-aspect="green" />
     </div>
   )
 }
