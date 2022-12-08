e1a64b4c5243e336765641f0f58015c083b42f65
with-heart <with.heart+git@pm.me>
2022-12-08 17:19:26 -0500

    add stoplight aspect-cycle behavior

--- src/stoplight.tsx
+++ src/stoplight.tsx
@@ -1,11 +1,48 @@
+import {useMachine} from '@xstate/react'
+import {createMachine} from 'xstate'
 import styles from './stoplight.module.scss'
 
+const stoplightMachine = createMachine({
+  initial: 'red',
+  states: {
+    red: {
+      after: {
+        3000: 'green',
+      },
+    },
+    amber: {
+      after: {
+        1000: 'red',
+      },
+    },
+    green: {
+      after: {
+        3000: 'amber',
+      },
+    },
+  },
+})
+
 export const Stoplight = () => {
+  const [state] = useMachine(stoplightMachine)
+
   return (
     <div className={styles.stoplight}>
-      <div className={styles.aspect} data-aspect="red" />
-      <div className={styles.aspect} data-aspect="amber" />
-      <div className={styles.aspect} data-aspect="green" />
+      <div
+        className={styles.aspect}
+        data-aspect="red"
+        data-active={state.matches('red')}
+      />
+      <div
+        className={styles.aspect}
+        data-aspect="amber"
+        data-active={state.matches('amber')}
+      />
+      <div
+        className={styles.aspect}
+        data-aspect="green"
+        data-active={state.matches('green')}
+      />
     </div>
   )
 }
